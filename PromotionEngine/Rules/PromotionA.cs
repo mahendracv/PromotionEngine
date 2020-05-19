83d1f316d859b24447a00724defe02b295997296
mahendracv <mahendracv.2007@gmail.com>
2020-05-19 17:24:06 +0530

    PromotionA implemented

--- PromotionEngine/Rules/PromotionA.cs
+++ PromotionEngine/Rules/PromotionA.cs
@@ -3,13 +3,32 @@ using PromotionEngine.ShoppingCart;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace PromotionEngine.Rules
 {
     public class PromotionA : IPromotion
     {
+        private string Description = "Buy 3 items of A's for 130";
+
+        char PromotionSKUID = 'A';
+        int PromotionQuantity = 3;
+        double PromotionPrice = 130;
+
         public Cart ApplyPromotion(Cart cart)
         {
+            CartItem product = cart.OrderItems.FindAll(x => x.SKUID.Equals(this.PromotionSKUID)).FirstOrDefault();
+            if (product != null)
+            {
+                int iligiableForPromotion = (product.Quantity / this.PromotionQuantity);
+
+                if (iligiableForPromotion > 0)
+                {
+                    product.Quantity -= (iligiableForPromotion * this.PromotionQuantity);
+                    product.QuantityWithPromotion += (iligiableForPromotion * this.PromotionQuantity);
+                    product.PromotionalPricing += (this.PromotionPrice * iligiableForPromotion);
+                }
+            }
             return cart;
         }
     }
--- PromotionEngine/ShoppingCart/CartItem.cs
+++ PromotionEngine/ShoppingCart/CartItem.cs
@@ -11,5 +11,8 @@ namespace PromotionEngine.ShoppingCart
     {
         public char SKUID { get; set; }
         public int Quantity { get; set; }
+        public double Price { get; set; }
+        public int QuantityWithPromotion { get; set; }
+        public double PromotionalPricing { get; set; }
     }
 }
