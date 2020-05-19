d4ba108d56436d05121dd2ba69e258b93b693d6b
mahendracv <mahendracv.2007@gmail.com>
2020-05-19 16:31:10 +0530

    Initial solution checkin

---
+++ PromotionEngineTest/PromotionEngineTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PromotionEngineTest
+{
+    public class PromotionEngineTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
